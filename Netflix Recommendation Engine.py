79acce2120ad374236b907c3ebb6de420e04b34e
Mila289 <mila.krasulina@gmail.com>
2025-01-04 14:03:52 +0700

    Устанавливаю в терминале pandas numpy nltk  и модули from nltk.tokenize import word_tokenize import numpy as np import pandas as pd import re import nltk import tkinter as tk from nltk.corpus import stopwords nltk.download('stopwords')

---
+++ Netflix Recommendation Engine.py
@@ -0,0 +1,8 @@
+from nltk.tokenize import word_tokenize
+import numpy as np
+import pandas as pd
+import re
+import nltk
+import tkinter as tk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
